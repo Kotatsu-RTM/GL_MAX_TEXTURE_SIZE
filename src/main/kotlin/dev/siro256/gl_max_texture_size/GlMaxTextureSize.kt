bd2f80177ba483851e36955da30442cf45e338c1
Siro_256 <siro@siro256.dev>
2023-02-22 14:44:42 +0900

    Create main class

---
+++ src/main/kotlin/dev/siro256/gl_max_texture_size/GlMaxTextureSize.kt
@@ -0,0 +1,22 @@
+package dev.siro256.gl_max_texture_size
+
+import org.lwjgl.glfw.GLFW
+import org.lwjgl.glfw.GLFWErrorCallback
+import org.lwjgl.opengl.GL
+import org.lwjgl.opengl.GL32
+
+object GlMaxTextureSize {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        GLFWErrorCallback.createPrint(System.err).set()
+        if (!GLFW.glfwInit()) throw RuntimeException("Unable to initialize GLFW")
+
+        GLFW.glfwDefaultWindowHints()
+        val window = GLFW.glfwCreateWindow(1, 1, "GL_MAX_TEXTURE_SIZE", 0L, 0L)
+
+        GLFW.glfwMakeContextCurrent(window)
+        GL.createCapabilities()
+
+        println(GL32.glGetInteger(GL32.GL_MAX_TEXTURE_SIZE))
+    }
+}
