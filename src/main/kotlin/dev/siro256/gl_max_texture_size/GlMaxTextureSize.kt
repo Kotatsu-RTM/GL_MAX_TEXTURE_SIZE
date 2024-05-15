d9b91974c0e66535d042b1385ca0b98d1c297de4
Siro_256 <siro@siro256.dev>
2024-05-15 22:39:36 +0900

    Add line width information

--- src/main/kotlin/dev/siro256/gl_max_texture_size/GlMaxTextureSize.kt
+++ src/main/kotlin/dev/siro256/gl_max_texture_size/GlMaxTextureSize.kt
@@ -18,11 +18,17 @@ object GlMaxTextureSize {
         GLFW.glfwMakeContextCurrent(window)
         GL.createCapabilities()
 
+        val aliasedLineWidthRange = FloatArray(2).apply { GL32.glGetFloatv(GL32.GL_ALIASED_LINE_WIDTH_RANGE, this) }
+        val smoothLineWidthRange = FloatArray(2).apply { GL32.glGetFloatv(GL32.GL_SMOOTH_LINE_WIDTH_RANGE, this) }
+
         println("""
             OpenGL Version: ${GL32.glGetInteger(GL32.GL_MAJOR_VERSION)}.${GL32.glGetInteger(GL32.GL_MINOR_VERSION)}
             Vendor: ${GL32.glGetString(GL32.GL_VENDOR)}
             Version: ${GL32.glGetString(GL32.GL_VERSION)}
             Max texture size: ${GL32.glGetInteger(GL32.GL_MAX_TEXTURE_SIZE)}
+            Aliased line width: ${aliasedLineWidthRange.joinToString(prefix = "[", postfix = "]")}
+            Smooth line width: ${smoothLineWidthRange.joinToString(prefix = "[", postfix = "]")}
+            Smooth line granularity: ${GL32.glGetFloat(GL32.GL_SMOOTH_LINE_WIDTH_GRANULARITY)}
         """.trimIndent())
     }
 }
